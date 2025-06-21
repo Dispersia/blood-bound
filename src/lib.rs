1e0244d4dc7afc2f314d238cc0b047c3e2f12353
Aaron Housh <dispersias@gmail.com>
2025-06-21 14:52:05 -0700

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,12 @@ pub fn main() {
         }))
         .insert_resource(WinitSettings::mobile())
         .add_systems(Startup, setup)
+        .add_systems(Update, move_player)
         .run();
 }
+
+#[derive(Component)]
+struct Player;
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
@@ -24,7 +28,36 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let texture = asset_server.load("character.png");
 
     commands.spawn((
-        Sprite::from_image(texture),
         Transform::from_xyz(0.0, 0.0, 0.0),
+        Sprite::from_image(texture),
+        Player,
     ));
+}
+
+fn move_player(
+    time: Res<Time>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Single<&mut Transform, With<Player>>,
+) {
+    let speed = 300.0;
+
+    let mut direction = Vec3::new(0.0, 0.0, 0.0);
+
+    if keyboard_input.pressed(KeyCode::KeyW) {
+        direction.y += 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        direction.y -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyD) {
+        direction.x += 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyA) {
+        direction.x -= 1.0;
+    }
+
+    query.translation += direction * speed * time.delta_secs();
 }
